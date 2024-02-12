90538d7c50749cb7352e08172274aaf3f7797fe7
Avik Pal <avikpal@mit.edu>
2024-02-11 20:05:56 -0500

    Add downgrade CI

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,10 @@
-using LuxAMDGPU, Test
+using Aqua, LuxAMDGPU, Test
 
 @testset "LuxAMDGPU" begin
     @test LuxAMDGPU.USE_AMD_GPU[] === nothing
 
     @test LuxAMDGPU.functional() isa Bool
+
+    Aqua.test_all(LuxAMDGPU; ambiguities=false)
+    Aqua.test_ambiguities(LuxAMDGPU)
 end
